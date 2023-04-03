0ecc2be86f90fc51de55c91d367ab9297d3a58e3
Barum-Niazi <barumkhan123@gmail.com>
2023-04-03 21:08:39 +0500

    finally did selection sort

--- Lab Assignment (Selection Sort).asm
+++ Lab Assignment (Selection Sort).asm
@@ -12,29 +12,33 @@ MOV DX, 0
 
 OUTERLOOP:
     MOV CX, 4
-    MOV SI, OFFSET DATA
-    MOV AL, [SI]
+    MOV SI, OFFSET DATA ; pointing SI to array
+    MOV AL, [SI] ; considering first element as biggest yet 
+    MOV BP, SI ; storing address of current biggest element
     SUB CX, DX
-    INC DX
+    INC DX     ; DX increases by number of passes
     CMP CX, 0 
     JG SORTLOOP
     ret
     
     SORTLOOP:
-        CMP AL, [SI + 1]
-        JL SWAP
-        INC SI 
-        LOOP SORTLOOP 
+        CMP AL, [SI + 1] ; comparing biggest to next elements
+        JL SWAP ; if biggest is less than the next it changes current biggest
+        INC SI  ; incrementing address
+        LOOP SORTLOOP
+        MOV BL, [SI]  ; moving last element of array to BL
+        MOV [BP], BL  ; storing last element at location of biggest
+        MOV [SI], AL  ; storing biggest at proper position
         JMP OUTERLOOP
         
      SWAP:
-        MOV AL, [SI + 1] 
+        MOV AL, [SI + 1] ; changing biggest element
         INC SI
-        
+        MOV BP, SI       ; storing address of new biggest element
         LOOP SORTLOOP
-        MOV BL, [SI + 1]
-        MOV [SI + 1], AL
-        MOV [SI], BL 
+        MOV BL, [SI]
+        MOV [BP], BL
+        MOV [SI], AL  
         JMP OUTERLOOP
         
 ret
---
+++ Lab Task (Masking lower-uper).asm
@@ -0,0 +1,15 @@
+
+org 100h
+
+.data
+
+.code
+
+MOV DL, 'a'
+OR DL, 00100000b
+
+ret
+
+
+
+
